98c0c2320ba8e709a1e3610fec16fbf0305694d5
Kadoia614 <miguel_moraes2001@hotmail.com>
2026-03-05 16:46:37 -0300

    feat: remove unused utility functions for email, password, CPF validation, and token verification

--- src/core/utils/verifyEmail.js
+++
@@ -1,7 +0,0 @@
-exports.verifyEmail = (email) => {
-  if (!/^[A-Z0-9._%+-]+@itapecerica+\.sp\.gov\.br$/i.test(email)) {
-    throw { code: 403, message: "Email inválido", ok: false, api: "User" };
-  } else {
-    return true;
-  }
-};
