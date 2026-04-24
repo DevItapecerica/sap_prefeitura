ce692348dc2a48bac75436df462fa93104c1aa8c
Kadoia614 <miguel_moraes2001@hotmail.com>
2026-04-24 12:22:41 -0300

    Remove dependency on makePermission in makeRoles factory

--- src/modules/roles/factories/makeRoles.ts
+++ src/modules/roles/factories/makeRoles.ts
@@ -1,11 +1,9 @@
 import { SequelizeRolesRepository } from "../../../infra/database/sequelize/repositories/sequelize.roles.repository.js";
-import { makePermission } from "../../permission/factories/makePermission.js";
 import RolesService from "../application/use-case/roles.use-case.js";
 
 export function makeRoles(logger: any): RolesService {
   return new RolesService(
     new SequelizeRolesRepository(),
-    makePermission(logger),
     logger,
   );
 }
