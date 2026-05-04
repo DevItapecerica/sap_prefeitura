5786f98128de47feff7babeb6a432efdc70ed06b
Kadoia614 <miguel_moraes2001@hotmail.com>
2026-05-04 11:40:44 -0300

    feat: simplify Carterinha model and DTO by removing unnecessary fields

--- src/modules/carterinhas/application/dto/carterinha.dto.ts
+++ src/modules/carterinhas/application/dto/carterinha.dto.ts
@@ -1,17 +1,7 @@
 export type CarterinhaDto = {
-  nome: string;
-  cpf: string;
-  nascimento: string;
-  telefone: string | null;
   emissao: string;
   validade: string | null;
-  rua: string;
-  bairro: string;
-  cidade: string;
-  uf: string;
-  cep: string;
-  numero: string;
-  complemento: string | null;
   setor: string;
   servico: string;
+  municipe_uuid: string;
 };
