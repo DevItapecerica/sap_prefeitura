e0223719fa73f5821a0d2265de6875978fabdb5a
Kadoia614 <miguel_moraes2001@hotmail.com>
2026-05-25 10:49:18 -0300

    feat: Update docker-compose to add alias for sap_prefeitura and enhance logging in LoggerResponse

--- src/core/config/logConfig.ts
+++ src/core/config/logConfig.ts
@@ -1,7 +1,10 @@
+import { type FastifyServerOptions } from "fastify";
+
 const logg = { translateTime: "HH:MM:ss", ignore: "hostname" };
 
-const logConfig = {
+const logConfig: FastifyServerOptions = {
   disableRequestLogging: true,
+  trustProxy: true,
   logger: {
     level: "info",
     transport: {
