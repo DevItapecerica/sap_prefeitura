f124e2aa5e2ac5736c0a1b84bb511c2404f3a440
Kadoia614 <miguel_moraes2001@hotmail.com>
2026-06-25 11:41:41 -0300

    feat: remove redundant authorization hook from service router

--- src/modules/services/intereface/router/services.router.ts
+++ src/modules/services/intereface/router/services.router.ts
@@ -6,14 +6,6 @@ import { authorizationFactory } from "../../../acess-controll/factory/makeAuthor
 
 const serviceRouter: FastifyPluginAsync = async (fastify) => {
   fastify.addHook("preHandler", AuthMiddleware.verifyJWT);
-  fastify.addHook("preHandler", async (request: FastifyRequest) => {
-    const verifyAuthorization = authorizationFactory(request.log);
-    await verifyAuthorization.authorize(
-      Number(request.user.id),
-      3,
-      request.method,
-    );
-  });
   
   const serviceProperties = {
     type: "object",
