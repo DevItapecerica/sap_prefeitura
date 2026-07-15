53b14e085d334c07ae3f577a16c813a436d58d8c
Kadoia614 <miguel_moraes2001@hotmail.com>
2026-07-15 17:30:08 -0300

    refactor: remove Chamado module and related components
    - Deleted Chamado DTOs, service, entity, repository, factory, controller, and router.
    - Cleaned up the entire Chamado module to streamline the codebase.

--- src/modules/chamados/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginAsync } from "fastify";
-import { chamadosRoutes } from "./interface/router/chamado.router.js";
-
-
-const chamadosModule: FastifyPluginAsync = async (fastify) => {
-  fastify.register(chamadosRoutes);
-};
-
-export default chamadosModule;
